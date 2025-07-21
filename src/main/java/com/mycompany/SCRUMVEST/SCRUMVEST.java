9518b093206729c4506a2303012bf078aa39da83
Daniel Orejuela <danielorejuelag@gmail.com>
2025-07-21 01:09:31 -0500

    Initial commit

---
+++ src/main/java/com/mycompany/SCRUMVEST/SCRUMVEST.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.SCRUMVEST;
+
+/**
+ *
+ * @author dorej
+ */
+public class SCRUMVEST {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
